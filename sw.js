d8a3615b811f6954b1cb9523cdce9a3cefc490a8
TTL-BioTech <ttlbio.service@gmail.com>
2026-05-21 11:45:14 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js - TTL Bio-Tech Admin (App Shell only) - v98.14.01
-const CACHE_NAME = 'ttl-admin-shell-v98.14.01';
+// sw.js - TTL Bio-Tech Admin (App Shell only) - v98.14.01-mapfix2
+const CACHE_NAME = 'ttl-admin-shell-v98.14.01-mapfix2';
 const CORE_ASSETS = [
   './',
   './manage_ttlbiotech_f3p4.html',
